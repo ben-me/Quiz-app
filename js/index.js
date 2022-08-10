ef4bf67c4ee9b064b6be1579c3b732a663683d5a
Ben Me <benedikt.mehler@mailbox.org>
2022-08-10 16:14:38 +0200

    add answerbutton function

--- js/index.js
+++ js/index.js
@@ -1,23 +1,3 @@
-const answerButton1 = document.querySelector('[data-js="button1"');
-const answerText1 = document.querySelector('[data-js="answer1"');
+import toggleAnswer from "./_answer.js";
 
-answerButton1.addEventListener("click", () => {
-  answerText1.classList.toggle("hidden");
-  if (answerButton1.innerHTML === "Show answer") {
-    answerButton1.innerHTML = "Hide answer";
-  } else {
-    answerButton1.innerHTML = "Show answer";
-  }
-});
-
-const bookmark = document.querySelector('[data-js="bookmark1"');
-let bookmarksaved = true;
-bookmark.addEventListener("click", () => {
-  if (bookmarksaved) {
-    bookmark.src = "../assets/bookmark_active.png";
-    bookmarksaved = !bookmarksaved;
-  } else {
-    bookmark.src = "../assets/bookmark.png";
-    bookmarksaved = true;
-  }
-});
+toggleAnswer();
