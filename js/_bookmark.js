b4adddad2585b73422f5f26157c56b1fe83e9dce
Ben Me <benedikt.mehler@mailbox.org>
2022-08-16 02:08:12 +0200

    nest bookmarkimg in buttons, differentiate styling of nav and bookmark-images

--- js/_bookmark.js
+++ js/_bookmark.js
@@ -2,9 +2,10 @@ export default function toggleBookmark() {
   const bookmarks = document.querySelectorAll('[data-js="bookmark"]');
   bookmarks.forEach((bookmark) => {
     let notSaved = true;
+    const bookmarkImage = bookmark.querySelector('[data-js="bookmarkImage"]');
     bookmark.addEventListener("click", () => {
       notSaved = !notSaved;
-      bookmark.src = notSaved
+      bookmarkImage.src = notSaved
         ? "./assets/bookmark.png"
         : "./assets/bookmark_active.png";
     });
