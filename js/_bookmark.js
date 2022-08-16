60e49b7b30d63bf37a330b7d08577cb5d6d45b03
Ben Me <benedikt.mehler@mailbox.org>
2022-08-16 17:15:30 +0200

    update bookmark JS and default homepage not hidden

--- js/_bookmark.js
+++ js/_bookmark.js
@@ -1,13 +1,13 @@
 export default function toggleBookmark() {
   const bookmarks = document.querySelectorAll('[data-js="bookmark"]');
   bookmarks.forEach((bookmark) => {
-    let notSaved = true;
+    let saved = false;
     const bookmarkImage = bookmark.querySelector('[data-js="bookmarkImage"]');
     bookmark.addEventListener("click", () => {
-      notSaved = !notSaved;
-      bookmarkImage.src = notSaved
-        ? "./assets/bookmark.png"
-        : "./assets/bookmark_active.png";
+      saved = !saved;
+      bookmarkImage.src = saved
+        ? "./assets/bookmark_active.png"
+        : "./assets/bookmark.png";
     });
   });
 }
