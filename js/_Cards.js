69cb2894c1e73d7d62e7fb8214fe7357dc8b1259
Ben Me <benedikt.mehler@mailbox.org>
2022-08-18 09:49:13 +0200

    create new JS for QuestionArray and add datajs attribute to elements

--- js/_Cards.js
+++ js/_Cards.js
@@ -1,37 +1,7 @@
+import { questionCards } from "./_questionarray.js";
+
 export default function Cards() {
   const homePage = document.querySelector('[data-js="homepage"]');
-
-  const questionCards = [
-    {
-      id: 1,
-      isBookmarked: false,
-      question: "How can you initialize a local repo with the terminal?",
-      answer: "git init",
-      hashtags: ["git", "cli", "vcs", "github"],
-    },
-    {
-      id: 2,
-      isBookmarked: false,
-      question: "What kind of selectors can you use in CSS?",
-      answer: "type, id, class, universal, pseudo elements and pseudo classes",
-      hashtags: ["css", "selectors", "styling"],
-    },
-    {
-      id: 3,
-      isBookmarked: false,
-      question:
-        "Which word allows you to have your grid-fields over several columns or rows?",
-      answer: "span",
-      hashtags: ["grid", "rows", "columns"],
-    },
-    {
-      id: 4,
-      isBookmarked: false,
-      question: "How can you become a Programmer for web development?",
-      answer: "Participate in a neue Fische Bootcamp",
-      hashtags: ["css", "html", "js", "react"],
-    },
-  ];
 
   questionCards.forEach((question) => {
     const card = document.createElement("section");
@@ -41,23 +11,26 @@ export default function Cards() {
     const bookmarkIcon = document.createElement("img");
     const bookmarkButton = document.createElement("button");
     const hashtaglist = document.createElement("ul");
-    let isShown = false;
-    let saved = false;
 
     card.classList.add("question");
+    card.setAttribute("data-js", "card");
 
     bookmarkButton.classList.add("question__bookmark");
+    bookmarkButton.setAttribute("data-js", "bookmark");
     bookmarkIcon.classList.add("question__bookmark--img");
     bookmarkIcon.src = "./assets/bookmark.png";
+    bookmarkIcon.setAttribute("data-js", "bookmarkImage");
 
     questionText.classList.add("question__text");
     questionText.textContent = question.question;
 
     answerButton.classList.add("question__button");
     answerButton.textContent = "Show answer";
+    answerButton.setAttribute("data-js", "button");
 
     answerText.classList.add("hidden");
     answerText.textContent = question.answer;
+    answerText.setAttribute("data-js", "answer");
 
     hashtaglist.classList.add("question__hashtags");
 
@@ -73,17 +46,5 @@ export default function Cards() {
     card.append(answerButton);
     card.append(answerText);
     card.append(hashtaglist);
-
-    answerButton.addEventListener("click", () => {
-      answerText.classList.toggle("hidden");
-      isShown = !isShown;
-      answerButton.textContent = isShown ? "Hide answer" : "Show answer";
-    });
-    bookmarkButton.addEventListener("click", () => {
-      saved = !saved;
-      bookmarkIcon.src = saved
-        ? "./assets/bookmark_active.png"
-        : "./assets/bookmark.png";
-    });
   });
 }
